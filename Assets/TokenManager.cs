a23ec6e90a2b5d75caff50c0da2e1870c1380b82
COM23a <user@FIH-COM23a101.fsu.edu>
2025-10-18 18:09:27 -0400

    changed particles to be by frame in update that way they follow the character instead of the token, still need to implement a timer and a destructor to limit clones

--- Assets/TokenManager.cs
+++ Assets/TokenManager.cs
@@ -5,7 +5,7 @@ using System;
 
 public class TokenManager : MonoBehaviour
 {
-    public ParticleSystem particleEffectPrefab;
+    public GameObject particleEffectPrefab;
     public static event Action OnTokenCollected;
     //public GameObject particleEffectPrefab;
     private int goalScore;
@@ -13,11 +13,15 @@ public class TokenManager : MonoBehaviour
     private List<GameObject> tokens;
     [SerializeField] private AudioClip collectSound;
     private AudioSource audioSource;
+    private Vector3 mouseWorldPosition;
+    private bool particlesActive = false;
+    private GameObject particles;
 
     // ... (Awake method is unchanged) ...
     void Awake()
     {
         tokens = new List<GameObject>();
+        mouseWorldPosition = new Vector3();
         foreach (Transform child in transform)
         {
             tokens.Add(child.gameObject);
@@ -32,15 +36,33 @@ public class TokenManager : MonoBehaviour
         }
         Debug.Log("Token Manager initialized. Goal Score: " + goalScore);
     }
+    private void Update()
+    {
+        Vector3 mouseScreenPosition = Input.mousePosition;
+        mouseScreenPosition.z = Camera.main.WorldToScreenPoint(transform.position).z;
 
+        // Convert mouse position from screen to world space
+        mouseWorldPosition = Camera.main.ScreenToWorldPoint(mouseScreenPosition);
+        particleEffectPrefab.transform.position = mouseWorldPosition; //gets positions for particle
+        //var mainModule = particleEffectPrefab.main;
+        if (particlesActive == false) 
+        {
+            particles = Instantiate(particleEffectPrefab, mouseWorldPosition, Quaternion.identity); //creates the clone
+            particles.GetComponent<ParticleSystem>().Play();
+
+            particlesActive = true;
+
+        }
+    }
+
+    //timer to reset particlesActive
 
     public void CollectToken(GameObject token)
     {
         token.SetActive(false);
-        
-        particleEffectPrefab.transform.position = token.transform.localPosition; //gets positions for particle
-        var mainModule = particleEffectPrefab.main;
+        var mainModule = particleEffectPrefab;
         mainModule.playOnAwake = true;
+
        //    var mainModule = targetParticleSystem.main; //access particle system
        //    mainModule.playOnAwake = true; //sets view on
         currentScore++;
@@ -51,7 +73,9 @@ public class TokenManager : MonoBehaviour
         if (currentScore < goalScore)
         {
             OnTokenCollected?.Invoke();
-            Instantiate(particleEffectPrefab, token.transform.position, Quaternion.identity); //creates the clone
+           
+            //make loop for if enabled set to mouse world position
+           
 
         }
 
