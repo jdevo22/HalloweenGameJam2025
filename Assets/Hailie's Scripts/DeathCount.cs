ad6c603408bff8537cda769bd58430b4e8336286
COM23a <user@FIH-COM23a101.fsu.edu>
2025-10-19 15:20:39 -0400

    implemented death counting system that saves between scenes and updates in time

--- Assets/Hailie's Scripts/DeathCount.cs
+++ Assets/Hailie's Scripts/DeathCount.cs
@@ -7,35 +7,30 @@ public class DeathCount : MonoBehaviour
     public static DeathCount Instance;
     public int deathCount = 0;
     public TextMeshProUGUI deathText;
-    private const string DeathCountKey = "TotalDeaths";
+    private const string DeathCountKey = "DeathCount";
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-      
+        deathCount = GetDeathCount();
     }
     public void YouDiedSucker()
     {
-        int deathCount = PlayerPrefs.GetInt(DeathCountKey, 0); //makes it work across scenes
+        int deathCount = PlayerPrefs.GetInt(DeathCountKey); //makes it work across scenes
+
+
       
         Debug.Log("Deaths: " + deathCount);
     }
     public static int GetDeathCount()
     {
-        return PlayerPrefs.GetInt(DeathCountKey, 0); //returns # if not 0
+        return PlayerPrefs.GetInt(DeathCountKey); //returns # if not 0
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (DeathCount.Instance != null)
-        {
-            deathText.text = "Deaths: " + DeathCount.Instance.deathCount.ToString();
-
-        }
-        else
-        {
-            deathText.text = "Deaths: 0";
-        }
+        deathCount = GetDeathCount();
+        deathText.text = "Deaths: " + deathCount.ToString();
     }
 }
