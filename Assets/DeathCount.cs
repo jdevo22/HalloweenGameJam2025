296ef6a7190f3020ff528a0834ffda0c5096757b
COM23a <user@FIH-COM23a101.fsu.edu>
2025-10-18 20:36:32 -0400

    Updated the death counter to use player prefs so it will hopefully work amonst scenes

--- Assets/DeathCount.cs
+++ Assets/DeathCount.cs
@@ -9,6 +9,7 @@ public class DeathCount : MonoBehaviour
     public static DeathCount Instance;
     public int deathCount = 0;
     public TextMeshProUGUI deathText;
+    private const string DeathCountKey = "TotalDeaths";
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -25,8 +26,13 @@ public class DeathCount : MonoBehaviour
     }
     public void YouDiedSucker()
     {
+        int deathCount = PlayerPrefs.GetInt(DeathCountKey, 0); //makes it work across scenes
         deathCount++;
         Debug.Log("Deaths: " + deathCount);
+    }
+    public static int GetDeathCount()
+    {
+        return PlayerPrefs.GetInt(DeathCountKey, 0); //returns # if not 0
     }
 
     // Update is called once per frame
